333206d52254f349df906b66d6909b41323b69cb
mobyte0 <mobyte25@gmail.com>
2017-07-18 12:50:36 -0400

    whole buncha updates

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,12 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ██╗███╗   ██╗██╗████████╗███████╗██╗      ;;
+;; ██║████╗  ██║██║╚══██╔══╝██╔════╝██║      ;;
+;; ██║██╔██╗ ██║██║   ██║   █████╗  ██║      ;;
+;; ██║██║╚██╗██║██║   ██║   ██╔══╝  ██║      ;;
+;; ██║██║ ╚████║██║   ██║██╗███████╗███████╗ ;;
+;; ╚═╝╚═╝  ╚═══╝╚═╝   ╚═╝╚═╝╚══════╝╚══════╝ ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (add-to-list 'load-path "~/.emacs.d")
@@ -19,6 +28,8 @@
 ;; Display line numbers
 (load "lisp/nlinum-1.7")
 (global-nlinum-mode)
+;; Display current line number
+(global-hl-line-mode t)
 ;; Display row and column numbers in the mode line
 (setq-default column-number-mode t)
 ;; Smooth scrolling
@@ -33,12 +44,12 @@
 
 ;;; Setup Appearance
 ;; Enable color theme
-(load "themes/nord/nord-theme")
+(load "themes/monokai-emacs/monokai-theme")
 ;; Enable color theme for new frames
 (add-hook 'after-make-frame-functions
 	  (lambda (frame)
 	    (select-frame frame)
-	    (load-theme 'nord t)))
+	    (load-theme 'monokai t)))
 ;; Disable visual scroll bar
 (add-to-list 'default-frame-alist
              '(vertical-scroll-bars . nil))
@@ -46,18 +57,18 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 ;; Highlight tail
-(load "lisp/highlight-tail")
-(require 'highlight-tail)
-(setq highlight-tail-colors '(
-			      ("#434c5e" . 0)
-			      ("#3b4252" . 40)
-			      ("#2e3440" . 80)
-			      ))
-(setq highlight-tail-steps 150
-      highlight-tail-timer 0.01)
-(setq highlight-tail-posterior-type 't)
-(highlight-tail-mode)
-(highlight-tail-reload)
+;(load "lisp/highlight-tail")
+;(require 'highlight-tail)
+;(setq highlight-tail-colors '(
+;			      ("#434c5e" . 0)
+;			      ("#3b4252" . 40)
+;			      ("#2e3440" . 80)
+;			      ))
+;(setq highlight-tail-steps 150
+;      highlight-tail-timer 0.01)
+;(setq highlight-tail-posterior-type 't)
+;(highlight-tail-mode)
+;(highlight-tail-reload)
 
 ;;; Indentation Settings
 ;; Enable smart tabs
@@ -69,16 +80,27 @@
 ;; Yes to y and No to n
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-;;; Loading magit and its dependencies
+;;; git packages
 (load "lisp/dash.el/dash")
 (load "lisp/dash.el/dash-functional.el")
 (load "lisp/with-editor/with-editor")
+(load "lisp/diff-hl/diff-hl")
+(load "lisp/diff-hl/diff-hl-flydiff")
 (add-to-list 'load-path "~/.emacs.d/lisp/magit/lisp")
 (require 'magit)
 (with-eval-after-load 'info
   (info-initialize)
   (add-to-list 'Info-directory-list
                "~/.emacs.d/lisp/magit/Documentation/"))
+(global-diff-hl-mode t)
+(diff-hl-flydiff-mode t)
+(setq diff-hl-side 'right)
+(setq diff-hl-fringe-bmp-function 'diff-hl-fringe-bmp-from-type)
+(custom-set-faces
+ '(diff-hl-change ((t (:foreground "background"))))
+ '(diff-hl-insert ((t (:foreground "background"))))
+ '(diff-hl-delete ((t (:foreground "background")))))
+(add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
 
 ;;; File management
 (setq make-backup-files nil)
@@ -88,4 +110,3 @@
 
 ;;; Other various emacs packages
 (load "lisp/try.el")
-
